0070f6205b683e86ebb781b24db3103fb0675e6b
sobiya-22 <sobiyashaikh22@gmail.com>
2025-11-05 20:25:24 +0530

    policy smart created,deployed and tested, functions in etherV6 created

--- backend/blockchain/policyRegistry.js
+++ backend/blockchain/policyRegistry.js
@@ -1,13 +1,80 @@
-// import { getContract } from "./config.js";
+import { getContract, loadAbi } from "./config.js";
 
-// const policyAddress = process.env.POLICY_REGISTRY_ADDRESS;
-// const abiPath = "artifacts/contracts/PolicyRegistry.sol/PolicyRegistry.json";
+const registryAddress = process.env.POLICY_REGISTRY_ADDRESS;
+const abiPath = "../config/PolicyABI.json";
 
-// export const registerPolicyOnChain = async (policyId, metadataUri) => {
-//   const policyRegistry = await getContract(policyAddress, abiPath);
-//   const tx = await policyRegistry.registerPolicy(policyId, metadataUri);
-//   await tx.wait();
-//   return tx.hash;
-// };
+console.log(await loadAbi("/config/PolicyABI.json"));
 
-// !---yet to be implemented --! do not touch this file
+/**
+ * Create a new policy on-chain
+ */
+export const createPolicyOnChain = async (customerAddress, customerDid, vcHash) => {
+  const registry = await getContract(registryAddress, abiPath);
+
+  const tx = await registry.createPolicy(customerAddress, customerDid, vcHash);
+  const receipt = await tx.wait();
+
+  return {
+    txHash: tx.hash,
+    policyId: Number(receipt.logs[0]?.args?.policyId || 0), // safest
+  };
+};
+
+/**
+ * Claim a policy (customer only)
+ */
+export const claimPolicyOnChain = async (policyId) => {
+  const registry = await getContract(registryAddress, abiPath);
+
+  const tx = await registry.claimPolicy(policyId);
+  await tx.wait();
+
+  return tx.hash;
+};
+
+/**
+ * Update VC hash (customer only)
+ */
+export const updatePolicyVcHashOnChain = async (policyId, newVcHash) => {
+  const registry = await getContract(registryAddress, abiPath);
+
+  const tx = await registry.updateVcHash(policyId, newVcHash);
+  await tx.wait();
+
+  return tx.hash;
+};
+
+/**
+ * Get complete policy details
+ */
+export const getPolicyOnChain = async (policyId) => {
+  const registry = await getContract(registryAddress, abiPath);
+
+  const data = await registry.getPolicy(policyId);
+
+  return {
+    customer: data[0],
+    customerDid: data[1],
+    vcHash: data[2],
+    isActive: data[3],
+  };
+};
+
+/**
+ * Check if a policy exists
+ */
+export const isPolicyExists = async (policyId) => {
+  const registry = await getContract(registryAddress, abiPath);
+
+  const customer = await registry.policyCustomer(policyId);
+  return customer !== "0x0000000000000000000000000000000000000000";
+};
+
+/**
+ * Get next policy ID (useful for UI previews)
+ */
+export const getNextPolicyId = async () => {
+  const registry = await getContract(registryAddress, abiPath);
+  return Number(await registry.nextPolicyId());
+};
+
